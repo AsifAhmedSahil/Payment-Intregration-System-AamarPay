82d4d5d071a410839e6efd232df747a185d7098f
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-24 00:45:05 +0600

    fixed

--- server/src/app/modules/payments/payment.utils.ts
+++ server/src/app/modules/payments/payment.utils.ts
@@ -2,42 +2,49 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const initiatePayment = async (paymentData:any) => {
-  const response = await axios.post(process.env.PAYMENT_URL!, {
+export const initiatePayment = async (paymentData: any) => {
+  try {
+    const response = await axios.post(process.env.PAYMENT_URL!, {
       store_id: process.env.STORE_ID,
       signature_key: process.env.SIGNAURE_KEY,
-    tran_id: paymentData.transactionId,
-    success_url: `http://localhost:5000/api/v1/payment/confirmation/?transactionId=${paymentData.transactionId}&status=success`,
-    fail_url: `http://localhost:5000/api/v1/payment/confirmation/?status=failed`,
-    cancel_url: "http://localhost:5173/",
-    amount: paymentData.totalPrice,
-    currency: "BDT",
-    desc: "Merchant Registration Payment",
-    cus_name: paymentData.customerName,
-    cus_email: paymentData.customerEmail,
-    cus_add1: paymentData.customerAddress,
-    cus_add2: paymentData.customerAddress,
-    cus_city: "N/A",
-    cus_state: "N/A",
-    cus_postcode: "N/A",
-    cus_country: "N/A",
-    cus_phone: paymentData.customerPhone,
-    type: "json",
-  });
+      tran_id: paymentData.transactionId,
+      success_url: `http://localhost:5000/api/v1/payment/confirmation/?transactionId=${paymentData.transactionId}&status=success`,
+      fail_url: `http://localhost:5000/api/v1/payment/confirmation/?status=failed`,
+      cancel_url: "http://localhost:5173/",
+      amount: paymentData.totalPrice,
+      currency: "BDT",
+      desc: "Merchant Registration Payment",
+      cus_name: paymentData.customerName,
+      cus_email: paymentData.customerEmail,
+      cus_add1: paymentData.customerAddress,
+      cus_add2: paymentData.customerAddress,
+      cus_city: "N/A",
+      cus_state: "N/A",
+      cus_postcode: "N/A",
+      cus_country: "N/A",
+      cus_phone: paymentData.customerPhone,
+      type: "json",
+    });
 
-  return response.data
+    return response.data;
+  } catch (error) {
+    throw new Error("payment initiation failed!");
+  }
 };
 
-export const verifyPayment = async (transactionId:string) =>{
-  const response = await axios.get(process.env.PAYMENT_VERIFY_URL!,{
-    params:{
-      store_id: process.env.STORE_ID,
-      signature_key: process.env.SIGNAURE_KEY,
-      type:"json",
-      request_id: transactionId
-    }
-  })
+export const verifyPayment = async (transactionId: string) => {
+  try {
+    const response = await axios.get(process.env.PAYMENT_VERIFY_URL!, {
+      params: {
+        store_id: process.env.STORE_ID,
+        signature_key: process.env.SIGNAURE_KEY,
+        type: "json",
+        request_id: transactionId,
+      },
+    });
 
-  return response.data
-
-}
+    return response.data;
+  } catch (error) {
+    throw new Error("Payment Validation Failed!");
+  }
+};

b4f20d56da8f7dc4f6210a99c53114000476d5f1
AsifAhmedSahil <asifahmedsahil.12@gmail.com>
2024-08-23 22:57:33 +0600

    Updating Payment Status in the Database.

--- server/src/app/modules/payments/payment.controller.ts
+++ server/src/app/modules/payments/payment.controller.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
+import { paymentServices } from "./payment.services";
 
 
 const confirmationController = async(req:Request,res:Response) =>{
+    const result = await paymentServices.confirmationService(req.query.transactionId as string)
     res.send(`<h1>Payment Success</h1>`)
 }
 
---
+++ server/src/app/modules/payments/payment.services.ts
@@ -0,0 +1,14 @@
+import orderModel from "../order/order.model"
+
+const confirmationService = async (transactionId : string) =>{
+    const result = await orderModel.findOneAndUpdate({transactionId},{
+        paymentStatus:"Paid"
+    })
+
+    return result
+
+}
+
+export const paymentServices = {
+    confirmationService
+}
